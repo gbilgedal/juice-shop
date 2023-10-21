fabace0e2f814c2f8ac4888d21f059a23b741d33
Bilge <gbilgedal@gmail.com>
2023-10-22 00:15:38 +0300

    fix xss try no 1

--- frontend/src/app/search-result/search-result.component.ts
+++ frontend/src/app/search-result/search-result.component.ts
@@ -143,12 +143,12 @@ export class SearchResultComponent implements OnDestroy, AfterViewInit {
   filterTable () {
     let queryParam: string = this.route.snapshot.queryParams.q
     if (queryParam) {
-      queryParam = queryParam.trim()
+      tempQueryParam = this.sanitizer.bypassSecurityTrustHtml(queryParam.trim())
       this.ngZone.runOutsideAngular(() => { // vuln-code-snippet hide-start
         this.io.socket().emit('verifyLocalXssChallenge', queryParam)
       }) // vuln-code-snippet hide-end
-      this.dataSource.filter = queryParam.toLowerCase()
-      this.searchValue = this.sanitizer.bypassSecurityTrustHtml(queryParam) // vuln-code-snippet vuln-line localXssChallenge xssBonusChallenge
+      this.dataSource.filter = tempQueryParam.toLowerCase()
+      this.searchValue = this.sanitizer.bypassSecurityTrustHtml(tempQueryParam) // vuln-code-snippet vuln-line localXssChallenge xssBonusChallenge
       this.gridDataSource.subscribe((result: any) => {
         if (result.length === 0) {
           this.emptyState = true
