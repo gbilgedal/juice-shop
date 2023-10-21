21ce07442d31d0035463979e1acf63dee771a045
Bilge <gbilgedal@gmail.com>
2023-10-21 23:02:42 +0300

    fix try no 1 for nosql code injection

--- routes/showProductReviews.ts
+++ routes/showProductReviews.ts
@@ -25,24 +25,27 @@ global.sleep = (time: number) => {
   }
 }
 
-module.exports = function productReviews () {
+module.exports = function productReviews() {
   return (req: Request, res: Response, next: NextFunction) => {
-    const id = utils.disableOnContainerEnv() ? Number(req.params.id) : req.params.id
+    const id = utils.disableOnContainerEnv() ? Number(req.params.id) : req.params.id;
 
-    // Measure how long the query takes, to check if there was a nosql dos attack
-    const t0 = new Date().getTime()
-    db.reviews.find({ $where: 'this.product == ' + id }).then((reviews: Review[]) => {
-      const t1 = new Date().getTime()
-      challengeUtils.solveIf(challenges.noSqlCommandChallenge, () => { return (t1 - t0) > 2000 })
-      const user = security.authenticatedUsers.from(req)
+    // Measure how long the query takes, to check if there was a NoSQL DoS attack
+    const t0 = new Date().getTime();
+
+    // Use a parameterized query to avoid NoSQL injection
+    db.reviews.find({ product: id }).then((reviews: Review[]) => {
+      const t1 = new Date().getTime();
+      challengeUtils.solveIf(challenges.noSqlCommandChallenge, () => (t1 - t0) > 2000);
+
+      const user = security.authenticatedUsers.from(req);
       for (let i = 0; i < reviews.length; i++) {
         if (user === undefined || reviews[i].likedBy.includes(user.data.email)) {
-          reviews[i].liked = true
+          reviews[i].liked = true;
         }
       }
-      res.json(utils.queryResultToJson(reviews))
+      res.json(utils.queryResultToJson(reviews));
     }, () => {
-      res.status(400).json({ error: 'Wrong Params' })
-    })
-  }
-}
+      res.status(400).json({ error: 'Wrong Params' });
+    });
+  };
+};
